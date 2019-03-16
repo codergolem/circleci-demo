da117ae04b33f22cd9d14333d709edb6202e002b
codergolem <mariocast@posteo.de>
2019-03-16 21:05:41 +0100

    Add terraform provider information

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "=0.11.11"
+}
+
+provider "aws" {
+  version = "2.2"
+  region = "eu-central-1"
+  allowed_account_ids = ["651625262782"]
+}
+
 //CircleCi technical user
 resource "aws_iam_user" "circle-ci" {
   name = "circle-ci"
