8758114fbeaac74af4d941b9146a00db23b86fad
codergolem <mariocast@posteo.de>
2019-03-19 15:39:54 +0100

    change the implementation to use a promise when creating the new access key

--- lambda/lambda.js
+++ lambda/lambda.js
@@ -1,32 +1,31 @@
 const request = require('request');
+const AWS = require('aws-sdk');
 const CIRCLE_CI_TOKEN = process.env.CIRCLE_CI_TOKEN;
 const circleciUrl = 'https://circleci.com/api/v1.1/project/github/codergolem/circleci-demo/envvar?circle-token=' + CIRCLE_CI_TOKEN;
 const iam = new AWS.IAM();
 
-exports.handler = (event,context) => {
+exports.handler = (event, context) => {
     const params = {
         UserName: "circle-ci"
     };
-
-    iam.createAccessKey(params, function(err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            const accessKeyId = data.AccessKey.AccessKeyId;
-            const secretKey = data.AccessKey.SecretAccessKey;
-            const payload = {
-                name : 'AWS_ACCESS_KEY',
-                value : accessKeyId,
+    iam.createAccessKey(params).promise().then(function (data) {
+        const accessKeyId = data.AccessKey.AccessKeyId;
+        console.log('keyId: ' + accessKeyId);
+        const secretKey = data.AccessKey.SecretAccessKey;
+        const payload = {
+            name: 'AWS_ACCESS_KEY',
+            value: accessKeyId,
+        };
+        request({
+            method: 'POST',
+            uri: circleciUrl,
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(payload)
+        }, (err, res, body) => {
+            if (err) {
+                return console.log(err);
             }
-            request({
-                method : 'POST',
-                uri : circleciUrl,
-                headers: {'content-type' : 'application/json' },
-                body : JSON.stringify(payload)
-            }, (err, res, body) => {
-                if (err) { return console.log(err); }
-                console.log(body.url);
-                console.log(body.explanation);
-            });
-        }
-    });
-}
+            console.log(body);
+        });
+    }).catch(error => console.log(error));
+};
